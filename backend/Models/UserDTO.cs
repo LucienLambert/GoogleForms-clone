6c524452f739a4213cbbfb233ecd3605ba3f3625
Benoît Penelle <benoit@penelle.net>
2024-10-20 15:23:00 +0200

    feat: version initiale

---
+++ backend/Models/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace prid_2425_a01.Models;
+
+public class UserDTO
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public Role Role { get; set; }
+    public DateTimeOffset? BirthDate { get; set; }
+    public string? Token { get; set; }
+}
