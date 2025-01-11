e015d18bdf248d1057568df8db6d253f7ae8254e
Quentin <quentinmaes96@gmail.com>
2025-01-12 00:57:45 +0100

    fix: view-instances admin rights

--- backend/Controllers/InstancesController.cs
+++ backend/Controllers/InstancesController.cs
@@ -64,7 +64,7 @@ public class InstancesController : ControllerBase
         
         if (instance == null)
             return NotFound();
-        if (user.Id != instance.UserId && user.Id != instance.Form.OwnerId)
+        if (user.Id != instance.UserId && user.Id != instance.Form.OwnerId && !user.IsInRole(Role.Admin))
             return Unauthorized();
         
         return Ok(_mapper.Map<Instance_With_Answers_And_Form_With_Questions_CompleteDTO>(instance));
@@ -395,7 +395,7 @@ public class InstancesController : ControllerBase
                                                         .Select(ufa=>ufa.UserId)
                                                         .ToList();
         
-        if (currentUser.Id != instancesToRemove[0].Form.OwnerId || editorIds.Contains(currentUser.Id))
+        if ((currentUser.Id != instancesToRemove[0].Form.OwnerId || editorIds.Contains(currentUser.Id) )&& !currentUser.IsInRole(Role.Admin))
             return Unauthorized();
 
         
@@ -430,7 +430,7 @@ public class InstancesController : ControllerBase
         var instancesToRemove = await _context.Instances
             .Where(i => i.FormId == formId && i.Completed != null).ToListAsync();
 
-        if(instancesToRemove.Any()){
+        if(!instancesToRemove.Any()){
             return NotFound(false);
         }
 
